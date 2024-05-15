1aec8d79e2fa6bcfc025def8c190d9b3413f5c01
Vyacheslav <failler815@gmail.com>
2024-05-15 14:52:24 +0300

    Задача 3.

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!')
+console.log('Welcome to the Brain Games!');
+
+import name from "../src/cli.js" ;
+
+let inpt = name
+console.log(`Hello, ${name}!`)
